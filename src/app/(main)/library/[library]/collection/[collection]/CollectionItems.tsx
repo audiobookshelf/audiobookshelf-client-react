ef5dd47bb18290cc74ee655a30bcf0f157e5570a
mikiher <mikiher@gmail.com>
2026-06-16 17:59:33 +0300

    CollectionItems: Rename from CollectionItemsClient; Refactor to use new providers, CollectionList, and CollectionBookshelf

---
+++ src/app/(main)/library/[library]/collection/[collection]/CollectionItems.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { SortableBookshelfOverlayProvider } from '@/contexts/SortableBookshelfOverlayContext'
+import { SortableCompilationProvider } from '@/contexts/SortableCompilationContext'
+import { useCompilationSortableContext } from '@/hooks/useCompilationSortableContext'
+import type { CollectionDisplayMode } from '@/hooks/useCollectionDisplayMode'
+import type { Collection, LibraryItem } from '@/types/api'
+import CollectionBookshelf from './CollectionBookshelf'
+import CollectionList from './CollectionList'
+
+interface CollectionItemsProps {
+  collection: Collection
+  displayMode: CollectionDisplayMode
+  mobileReorderActive: boolean
+  orderedBooks: LibraryItem[]
+  setOrderedBooks: (next: LibraryItem[]) => void
+  onItemRemoved: (libraryItemId: string, episodeId?: string | null) => void
+}
+
+export default function CollectionItems({
+  collection,
+  displayMode,
+  mobileReorderActive,
+  orderedBooks,
+  setOrderedBooks,
+  onItemRemoved
+}: CollectionItemsProps) {
+  const { showReorder, sortableCompilation, bookshelfOverlayMode } = useCompilationSortableContext(
+    collection.id,
+    'collection',
+    mobileReorderActive,
+    onItemRemoved
+  )
+
+  return (
+    <SortableCompilationProvider value={sortableCompilation}>
+      {displayMode === 'bookshelf' ? (
+        <SortableBookshelfOverlayProvider overlayMode={bookshelfOverlayMode}>
+          <CollectionBookshelf collection={collection} orderedBooks={orderedBooks} setOrderedBooks={setOrderedBooks} showReorder={showReorder} />
+        </SortableBookshelfOverlayProvider>
+      ) : (
+        <CollectionList collection={collection} orderedBooks={orderedBooks} setOrderedBooks={setOrderedBooks} showReorder={showReorder} />
+      )}
+    </SortableCompilationProvider>
+  )
+}
