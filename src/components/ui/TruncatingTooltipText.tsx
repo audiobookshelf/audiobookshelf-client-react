4f32211f5b5934031f76beae425b0c447245e86d
mikiher <mikiher@gmail.com>
2026-06-27 12:25:09 +0300

    TruncatingTooltipText: Accept customizable tooltip position

--- src/components/ui/TruncatingTooltipText.tsx
+++ src/components/ui/TruncatingTooltipText.tsx
@@ -7,15 +7,16 @@ import { mergeClasses } from '@/lib/merge-classes'
 interface TruncatingTooltipTextProps {
   text: string
   className: string
+  position?: 'top' | 'bottom' | 'left' | 'right'
 }
 
 /**
  * If the text is truncated, a tooltip will be shown.
  */
-export default function TruncatingTooltipText({ text, className }: TruncatingTooltipTextProps) {
+export default function TruncatingTooltipText({ text, className, position = 'top' }: TruncatingTooltipTextProps) {
   const { ref, isTruncated } = useTruncation(text, false)
   return (
-    <Tooltip text={text} position="top" disabled={!isTruncated} className="block w-full">
+    <Tooltip text={text} position={position} disabled={!isTruncated} className="block w-full">
       <p ref={ref} className={mergeClasses('truncate', className)}>
         {text}
       </p>
