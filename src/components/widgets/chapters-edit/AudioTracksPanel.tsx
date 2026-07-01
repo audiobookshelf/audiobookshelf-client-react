5238dcff9631e74cead00816fbe580fb95610e2e
mikiher <mikiher@gmail.com>
2026-07-01 13:33:19 +0300

    Fix filename truncation issues; show full names via a lazy tooltip on hover.

--- src/components/widgets/chapters-edit/AudioTracksPanel.tsx
+++ src/components/widgets/chapters-edit/AudioTracksPanel.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Btn from '@/components/ui/Btn'
+import LazyTruncatingTooltipText from '@/components/ui/LazyTruncatingTooltipText'
 import Tooltip from '@/components/ui/Tooltip'
 import { useTypeSafeTranslations } from '@/hooks/useTypeSafeTranslations'
 import { secondsToTimestamp } from '@/lib/datefns'
@@ -18,7 +19,7 @@ export default function AudioTracksPanel({ tracks, currentTrackIndex, isPlayingC
   const t = useTypeSafeTranslations()
 
   return (
-    <div className="w-full max-w-3xl px-2 py-4 min-[1120px]:max-w-xl">
+    <div className="w-full max-w-3xl overflow-x-hidden px-2 py-4 min-[1120px]:max-w-xl">
       <div className="mb-4 flex items-center py-1">
         <p className="text-lg font-semibold">{t('HeaderAudioTracks')}</p>
         <div className="grow" />
@@ -31,15 +32,15 @@ export default function AudioTracksPanel({ tracks, currentTrackIndex, isPlayingC
       </div>
 
       <div className="text-foreground-muted mb-2 flex text-xs font-semibold uppercase">
-        <div className="grow min-[1120px]:max-w-64 xl:max-w-sm">{t('LabelFilename')}</div>
+        <div className="min-w-0 grow min-[1120px]:max-w-64 xl:max-w-sm">{t('LabelFilename')}</div>
         <div className="w-20">{t('LabelDuration')}</div>
         <div className="hidden w-20 text-center md:block">{t('HeaderChapters')}</div>
       </div>
 
       {tracks.map((track) => (
         <div key={track.ino} className={mergeClasses('flex items-center py-2', currentTrackIndex === track.index && isPlayingChapter && 'bg-success/10')}>
-          <div className="grow pr-2 min-[1120px]:max-w-64 xl:max-w-sm">
-            <p className="truncate text-xs">{track.metadata.filename}</p>
+          <div className="min-w-0 grow overflow-hidden pr-2 min-[1120px]:max-w-64 xl:max-w-sm">
+            <LazyTruncatingTooltipText text={track.metadata.filename} className="text-xs" maxWidth={400} />
           </div>
           <div className="w-20" style={{ minWidth: 80 }}>
             <p className="text-foreground-muted font-mono text-xs">{secondsToTimestamp(Math.round(track.duration))}</p>
