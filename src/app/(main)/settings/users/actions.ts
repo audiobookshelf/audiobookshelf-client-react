ff3e37ca64bc98d09d7078f97d748a3c27934d67
mikiher <mikiher@gmail.com>
2026-07-19 11:56:58 +0300

    Add user settings server actions

--- src/app/(main)/settings/users/actions.ts
+++ src/app/(main)/settings/users/actions.ts
@@ -1,12 +1,44 @@
 'use server'
 
 import * as api from '@/lib/api'
-import { Library } from '@/types/api'
+import { GetListeningSessionsResponse, Library, ListeningStats, UserAccountPayload } from '@/types/api'
 import { revalidatePath } from 'next/cache'
 
 export async function deleteUser(userId: string): Promise<void> {
   await api.deleteUser(userId)
   revalidatePath('/settings/users')
+}
+
+export async function createUser(payload: UserAccountPayload) {
+  const result = await api.createUser(payload)
+  revalidatePath('/settings/users')
+  return result
+}
+
+export async function updateUser(userId: string, payload: UserAccountPayload) {
+  const result = await api.updateUser(userId, payload)
+  if (result.user.accessToken) {
+    const currentUser = await api.getCurrentUser()
+    if (currentUser.user.id === userId) {
+      await api.persistAccessTokenInCookies(result.user.accessToken)
+    }
+  }
+  revalidatePath('/settings/users')
+  revalidatePath(`/settings/users/${userId}`)
+  return result
+}
+
+export async function unlinkUserOpenId(userId: string): Promise<void> {
+  await api.unlinkUserOpenId(userId)
+  revalidatePath('/settings/users')
+}
+
+export async function fetchUserListeningStats(userId: string): Promise<ListeningStats> {
+  return api.getUserListeningStats(userId)
+}
+
+export async function fetchUserListeningSessions(userId: string, queryParams?: string): Promise<GetListeningSessionsResponse> {
+  return api.getUserListeningSessions(userId, queryParams)
 }
 
 export async function fetchLibraries(): Promise<Library[]> {
