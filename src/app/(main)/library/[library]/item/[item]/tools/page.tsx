789571b6f9248129218f01750c7e8a01319af1ee
mikiher <mikiher@gmail.com>
2026-08-12 08:29:22 +0300

    Add /library empty home when no libraries are available

--- src/app/(main)/library/[library]/item/[item]/tools/page.tsx
+++ src/app/(main)/library/[library]/item/[item]/tools/page.tsx
@@ -9,8 +9,8 @@ export default async function ToolsPage({ params }: { params: Promise<{ item: st
   const { item: itemId } = await params
   const [libraryItem, currentUser] = await getData(getLibraryItemOrNotFound(itemId, true), getCurrentUser())
 
-  if (!currentUser) {
-    redirect('/')
+  if (!libraryItem || !currentUser) {
+    redirect('/library')
   }
 
   const itemPath = `/library/${libraryItem.libraryId}/item/${libraryItem.id}`
