4f5f372d5537857c1befabbca4c67c9ce264ffc4
manan1820 <77871695+manan1820@users.noreply.github.com>
2026-08-18 23:58:35 +1200

    Added titles in layout.tsx
    added titles in layout.tsx of library, components, settings, upload

--- src/app/(main)/components_catalog/layout.tsx
+++ src/app/(main)/components_catalog/layout.tsx
@@ -1,12 +1,17 @@
+import { getTypeSafeTranslations } from '@/lib/getTypeSafeTranslations'
 import type { Metadata } from 'next'
 import '../../../assets/globals.css'
 import { ComponentsCatalogProvider } from '../../../contexts/ComponentsCatalogContext'
 import { getData, getLibraries } from '../../../lib/api'
 import AppBarLoader from '../AppBarLoader'
 
-export const metadata: Metadata = {
-  title: 'audiobookshelf - Components Catalog',
-  description: 'Components catalog for audiobookshelf client'
+export async function generateMetadata(): Promise<Metadata> {
+  const t = await getTypeSafeTranslations()
+
+  return {
+    title: t('TitleComponents'),
+    description: 'Components catalog for audiobookshelf client'
+  }
 }
 
 export default async function ComponentsCatalogLayout({ children }: Readonly<{ children: React.ReactNode }>) {
